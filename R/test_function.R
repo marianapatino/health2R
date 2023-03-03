3afd8cd0be5f645b913459c34ef69cffdef06077
Mariana Patino <m.patino@zeppelin-university.net>
2023-03-03 12:00:13 +0100

    initial commits and package template

---
+++ R/test_function.R
@@ -0,0 +1,16 @@
+#' xxx
+#'
+#' @param xx xxxx
+#' @param xxxx yyyyy
+#' @return zzzzz
+#' @references lalal
+#' @note note
+#' @examples
+#' options(tidyverse.quiet = TRUE)
+#' library(tidyverse)
+#' library(health2R)
+#' @export
+test_function <- function(xx,xxx){
+
+  return(123)
+}
